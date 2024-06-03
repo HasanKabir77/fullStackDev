14815b777b2ad2cd83f7fe5c69253428131b943b
Your NameHasanKabir77 <you@example.comhasankabir0720@gmail.com>
2024-06-03 19:44:37 +0600

    03-06-24 office work

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [, setJokes] = useState
+  const [, setJokes] = useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:7700/jokes')
+    .then((response)=>{
+      setJokes(response.data)
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  })
 
   return (
     <>
       <h1>Full Stack Dev. with Hitesh </h1>
       <h2>(chai aur code..!)</h2>
+
+      <p>Jokes: {jokes.length}</p>
+
+      {
+        jokes.map((joke, index)=>{
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.contant}</p>
+          </div>
+          
+        })
+      }
     </>
   )
 }
